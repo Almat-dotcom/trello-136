41bac077d62c945f7b790b5b168097158fd78ef6
denis.valyayev <ValyayevD@kacd.kz>
2022-11-04 16:59:41 +0600

    Added realm controller base project

---
+++ controllers/realm-controller/src/main/java/kz/kacd/sso/realmcontroller/RealmControllerApplication.java
@@ -0,0 +1,16 @@
+package kz.kacd.sso.realmcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class RealmControllerApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(RealmControllerApplication.class, args);
+    }
+}
