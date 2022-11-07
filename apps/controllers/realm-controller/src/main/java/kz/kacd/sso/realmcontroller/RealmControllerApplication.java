63176c3e8460cf157b03cfed65d56e72e102ecf8
denis.valyayev <ValyayevD@kacd.kz>
2022-11-07 16:07:21 +0600

    Added realm controller to sso deployment
    Added hallo endpoint to hold spring app up and running
    Added helm chart templates to deploy realm-controller
    Added realm-controller image substitution to gitlab ci

--- apps/controllers/realm-controller/src/main/java/kz/kacd/sso/realmcontroller/RealmControllerApplication.java
+++ apps/controllers/realm-controller/src/main/java/kz/kacd/sso/realmcontroller/RealmControllerApplication.java
@@ -2,8 +2,12 @@ package kz.kacd.sso.realmcontroller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAsync
 @EnableScheduling
@@ -12,5 +16,18 @@ public class RealmControllerApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(RealmControllerApplication.class, args);
+    }
+
+    /**
+     * This endpoint serves simple method to lock spring context and up web server.
+     */
+    @RestController
+    @RequestMapping("/hallo")
+    public static class HalloEndpoint {
+
+        @GetMapping(value = "", produces = MediaType.TEXT_PLAIN_VALUE)
+        public String hallo() {
+            return "Hallo";
+        }
     }
 }
