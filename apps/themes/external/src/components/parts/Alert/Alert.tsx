e2a2a66f8093d786e314cbf4b938aece7593570e
denis.valyayev <ValyayevD@kacd.kz>
2022-12-07 14:59:00 +0600

    Fixed alert message and failed select value

--- apps/themes/external/src/components/parts/Alert/Alert.tsx
+++ apps/themes/external/src/components/parts/Alert/Alert.tsx
@@ -43,10 +43,12 @@ const Alert = ({ type, message, i18n }: { type: "success" | "warning" | "error" 
         }
     }
 
+    const messages = message.split('<br>').map((it) => <span className="font-bold">{advancedMsgStr(message) || message}</span>);
+
     return (
-        <div className={ `flex items-center px-2 py-3 ${bg} ${border} border-l-4 ${color} text-sm text-left` }>
-            <img className="mr-2" alt="severity" src={icon}/>
-            <span className={ `font-bold` }>{advancedMsgStr(message) || message}</span>
+        <div className={`flex items-center px-2 py-3 ${bg} ${border} border-l-4 ${color} text-sm text-left`}>
+            <img className="mr-2" alt="severity" src={icon} />
+            {messages}
         </div>
     );
 };
--- apps/themes/external/src/components/parts/Input/InputSelect.tsx
+++ apps/themes/external/src/components/parts/Input/InputSelect.tsx
@@ -52,13 +52,14 @@ const InputSelect = ({ fieldName, label, options, value, error, onValueChange }:
             <div id="floating_outlined" className={(!changed && error ? "border-red-600" : "border-gray-300") + " flex w-full text-gray-900 rounded-lg border-2 dark:text-white dark:border-gray-600 dark:focus:border-primary focus:ring-0 focus:border-primary peer"}>
                 <input
                     className="h-full w-full appearance-none peer block cursor-pointer px-2.5 py-1 pt-4 bg-transparent focus:outline-none"
-                    id={fieldName}
-                    name={fieldName}
+                    id={fieldName + "_label"}
+                    name={fieldName + "_label"}
                     value={current.label}
                     onChange={() => { }}
                     onClick={() => setExpanded(!expanded)}
                     readOnly
                 />
+                <input type="hidden" id={fieldName} name={fieldName} value={current.value} onChange={() => {}}/>
                 <div>
                     <button
                         type="button"
