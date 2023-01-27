1876338495dd3d33f99e1c9055506bde1e2f1f9e
denis.valyayev <ValyayevD@kacd.kz>
2023-01-27 15:25:17 +0600

    Added distribution between runners

--- apps/themes/external/src/components/parts/Header/Header.tsx
+++ apps/themes/external/src/components/parts/Header/Header.tsx
@@ -8,10 +8,10 @@ const Header = ({ current, langs, i18n }: { current: string, langs: any[], i18n:
     return (
         <>
             <div className="fixed flex items-center justify-between z-50 w-full bg-white border-b-2 border-gray-200">
-                <div className="my-6 pl-4 xl:pl-80">
+                <div className="my-6 pl-4 xl:pl-72">
                     <Logo />
                 </div>
-                <div className="flex items-center pr-4 xl:pr-80">
+                <div className="flex items-center pr-4 xl:pr-72">
                     <a className="flex items-center mr-4 text-dark-text hover:text-primary-focus" href="/foo">
                         <img className="inline-block mr-2" alt="faq" src={FAQ}/><span className="hidden md:inline-block">{msgStr("faq")}</span>
                     </a>
