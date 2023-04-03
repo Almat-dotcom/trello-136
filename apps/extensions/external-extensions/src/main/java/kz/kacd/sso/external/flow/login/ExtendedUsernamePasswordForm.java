f82decfe13d3cb3733178724d1f6ead21e301292
denis.valyayev <ValyayevD@kacd.kz>
2023-04-03 15:07:51 +0600

    Added debugging to iin login

--- apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
+++ apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
@@ -49,9 +49,11 @@ public class ExtendedUsernamePasswordForm extends UsernamePasswordForm implement
             public void action(AuthenticationFlowContext context) {
                 String username = extractUsername(context);
                 boolean isIin = isIin(username);
+                log.infof("username: {}; IsIin: {}", isIin);
                 if (isIin) {
                     rewriteContextUsername(context, username);
                 }
+                log.infof("Extracted changed username {}", extractUsername(context));
 
                 ExtendedUsernamePasswordForm.super.action(context);
 
