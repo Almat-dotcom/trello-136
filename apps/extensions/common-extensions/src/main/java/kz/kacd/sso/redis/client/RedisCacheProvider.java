fb3282a8e3b18b7acc81556ea1f8c0adc23f7b52
denis.valyayev <ValyayevD@kacd.kz>
2023-04-10 17:14:34 +0600

    Removed redis integration

--- apps/extensions/common-extensions/src/main/java/kz/kacd/sso/redis/client/RedisCacheProvider.java
+++
@@ -1,8 +0,0 @@
-package kz.kacd.sso.redis.client;
-
-import org.keycloak.provider.Provider;
-
-public interface RedisCacheProvider extends Provider {
-
-    <T> RedisCache<T> create(CacheConfig<T> config);
-}
--- apps/extensions/common-extensions/src/main/java/kz/kacd/sso/redis/client/RedisCacheProviderFactory.java
+++
@@ -1,6 +0,0 @@
-package kz.kacd.sso.redis.client;
-
-import org.keycloak.provider.ProviderFactory;
-
-public interface RedisCacheProviderFactory extends ProviderFactory<RedisCacheProvider> {
-}
