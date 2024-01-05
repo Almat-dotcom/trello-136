294e78b948574b2141d8413bbe19582385e894f2
denis.valyayev <ValyayevD@kacd.kz>
2024-01-05 10:19:19 +0600

    LK-996: Added full name to update profile endpoint

--- apps/extensions/common-extensions/src/main/java/kz/kacd/sso/resource/profile/UpdateProfileRequest.java
+++ apps/extensions/common-extensions/src/main/java/kz/kacd/sso/resource/profile/UpdateProfileRequest.java
@@ -4,14 +4,9 @@ public class UpdateProfileRequest {
 
     private String newEmail;
     private String newPhoneNumber;
-
-    public UpdateProfileRequest() {
-    }
-
-    public UpdateProfileRequest(String newEmail, String newPhoneNumber) {
-        this.newEmail = newEmail;
-        this.newPhoneNumber = newPhoneNumber;
-    }
+    private String firstName;
+    private String lastName;
+    private String middleName;
 
     public String getNewEmail() {
         return newEmail;
@@ -27,5 +22,29 @@ public class UpdateProfileRequest {
 
     public void setNewPhoneNumber(String newPhoneNumber) {
         this.newPhoneNumber = newPhoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
     }
 }
