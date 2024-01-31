ea20eb866e1f2d44fc0298805b57cadd2d5f52f9
MussinA <MussinA@kacd.kz>
2024-01-31 16:50:14 +0600

    LK-1060: fixed lint

--- apps/themes/external/src/components/parts/Carousel/Carousel.tsx
+++ apps/themes/external/src/components/parts/Carousel/Carousel.tsx
@@ -1,10 +1,7 @@
 import News3 from "./news_3.jpg";
-import { useState } from "react";
 import { I18n } from "lib/i18n";
 
 const Carousel = ({ i18n } : { i18n: I18n }) => {
-    const { msgStr } = i18n;
-    const [page, setPage] = useState(0);
     return (
         <div className="w-5/6 py-8">
             <div>
@@ -12,6 +9,7 @@ const Carousel = ({ i18n } : { i18n: I18n }) => {
                     <div className="m-3 w-11/12">
                         <img
                             src={News3}
+                            alt={""}
                             className="w-full rounded-3xl" />
                     </div>
                 </div>
