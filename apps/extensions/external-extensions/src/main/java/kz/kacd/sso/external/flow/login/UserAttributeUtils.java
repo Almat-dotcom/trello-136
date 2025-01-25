856d4f979ac1c7510282741b724af4cbb191d778
AlmatS <SagandykovA@kacd.kz>
2025-01-25 14:53:10 +0500

    LoginConfigTotp form after login form

--- apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/UserAttributeUtils.java
+++ apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/UserAttributeUtils.java
@@ -1,14 +1,18 @@
 package kz.kacd.sso.external.flow.login;
 
+import org.jboss.logging.Logger;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 
 public class UserAttributeUtils {
+    private static final Logger LOG = Logger.getLogger(ConditionalOTPAuthenticator.class);
+
 
     private static final String OTP_ENABLED_ATTRIBUTE = "otp_enabled";
 
     public static boolean isOtpEnabled(UserModel user) {
+        LOG.info("Check otp");
         String otpEnabled = user.getFirstAttribute(OTP_ENABLED_ATTRIBUTE);
         return otpEnabled != null && otpEnabled.equalsIgnoreCase("true");
     }
