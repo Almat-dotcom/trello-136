5f5df05f26e74b2e92d6ae33e675f2e6b0ab1d86
AlmatS <SagandykovA@kacd.kz>
2025-02-05 01:54:51 +0500

    INFR-862: AFR.Forgot Password hidden username.

--- apps/themes/afr/src/components/UpdatePassword/UpdatePassword.tsx
+++ apps/themes/afr/src/components/UpdatePassword/UpdatePassword.tsx
@@ -78,7 +78,7 @@ const UpdatePassword = memo((props: PageProps<KcContext_UpdatePassword, I18n>) =
                             </label>
                             <input
                                 id="password"
-                                name="password"
+                                name="passwordNew"
                                 type="password"
                                 className={clsx(kcProps.kcInputClass)}
                                 value={password}
@@ -99,7 +99,7 @@ const UpdatePassword = memo((props: PageProps<KcContext_UpdatePassword, I18n>) =
                             </label>
                             <input
                                 id="password-confirm"
-                                name="passwordConfirm"
+                                name="passwordNewConfirm"
                                 type="password"
                                 className={clsx(kcProps.kcInputClass)}
                                 value={passwordConfirm}
