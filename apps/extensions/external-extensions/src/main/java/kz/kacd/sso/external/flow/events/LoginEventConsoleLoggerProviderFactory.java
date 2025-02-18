f0e26aa10fb129eb731379278d69c7171305b0d1
KulbayevaZH <kulbayevaZH@kacd.kz>
2025-02-18 16:54:41 +0500

    LK-1800: Add last login time

--- apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/events/LoginEventConsoleLoggerProviderFactory.java
+++ apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/events/LoginEventConsoleLoggerProviderFactory.java
@@ -1,36 +1,35 @@
 package kz.kacd.sso.external.flow.events;
 
-import com.google.auto.service.AutoService;
 import org.keycloak.Config;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
-@AutoService(EventListenerProviderFactory.class)
-public class LoginEventListenerProviderFactory implements EventListenerProviderFactory {
-
-    public static final String ID = "login-event-listener";
+public class LoginEventConsoleLoggerProviderFactory implements EventListenerProviderFactory {
 
     @Override
     public EventListenerProvider create(KeycloakSession session) {
-        return new LoginEventListenerProvider(session);
+        return new LoginEventConsoleLogger();
     }
 
     @Override
     public void init(Config.Scope config) {
+
     }
 
     @Override
     public void postInit(KeycloakSessionFactory factory) {
+
     }
 
     @Override
     public void close() {
+
     }
 
     @Override
     public String getId() {
-        return ID;
+        return "login-event-console-logger";
     }
 }
