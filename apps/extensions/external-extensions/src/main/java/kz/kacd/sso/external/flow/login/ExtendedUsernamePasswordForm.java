a9d872d8f728a753637de414d4b4fd9e4a681905
Almat-dotcom <asagyndykov06@gmail.com>
2025-06-17 14:01:30 +0500

    Revert "INFR-1038: Keycloak 22 to 23"
    This reverts commit 7a86f236e37abf7b444dbc5da79230746181162f.

--- apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
+++ apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
@@ -1,6 +1,5 @@
 package kz.kacd.sso.external.flow.login;
 
-import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import kz.kacd.sso.external.model.page.ExternalLoginPage;
 import kz.kacd.sso.external.model.page.ExternalRegistrationPage;
@@ -59,7 +58,7 @@ public class ExtendedUsernamePasswordForm extends UsernamePasswordForm implement
                 boolean isIin = isIin(username);
                 log.debug("Aza isIin is " + isIin);
                 if (isIin) {
-                    rewriteContextUsername(context, username, true);
+                    rewriteContextUsername(context, username);
                 }
 
                 ExtendedUsernamePasswordForm.super.action(context);
@@ -84,20 +83,10 @@ public class ExtendedUsernamePasswordForm extends UsernamePasswordForm implement
         return username.length() == 12 && username.matches("\\d+");
     }
 
-    private void rewriteContextUsername(AuthenticationFlowContext ctx,
-                                        String iin,
-                                        boolean toPhysical) {
-
-        String login = toPhysical
-                ? iin + "-" + ExternalRegistrationPage.CLIENT_PHYSICAL
-                : iin;
-
-        MultivaluedMap<String,String> form = ctx.getHttpRequest().getDecodedFormParameters();
-        form.putSingle(AuthenticationManager.FORM_USERNAME,  login);
-
-
-        ctx.getAuthenticationSession()
-                .setAuthNote(AuthenticationManager.FORM_USERNAME, login);
+    private void rewriteContextUsername(AuthenticationFlowContext context, String iin) {
+        log.debug("Aza Rewrite context username: " + iin);
+        String username = iin + "-" + ExternalRegistrationPage.CLIENT_PHYSICAL;
+        setUsername(context, username);
     }
 
     private void rewriteContextIin(AuthenticationFlowContext context, String iin) {
