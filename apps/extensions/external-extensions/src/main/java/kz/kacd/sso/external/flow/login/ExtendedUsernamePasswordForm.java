5a6ca3a3ad021ea1fad3eed3e2e3df753aaa4074
Almat-dotcom <asagyndykov06@gmail.com>
2025-06-17 14:01:28 +0500

    Revert "INFR-1038: Keycloak 22 to 23"
    This reverts commit b921182f3acfbfc0b75671341b7fe0c15be494ab.

--- apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
+++ apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
@@ -81,7 +81,6 @@ public class ExtendedUsernamePasswordForm extends UsernamePasswordForm implement
     }
 
     private boolean isIin(String username) {
-        username = username.trim();
         return username.length() == 12 && username.matches("\\d+");
     }
 
