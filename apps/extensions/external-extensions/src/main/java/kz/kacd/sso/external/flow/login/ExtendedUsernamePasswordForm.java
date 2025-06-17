1f2a9ef2481e00ad4ddf15b41545f669faa8a1c9
Almat-dotcom <asagyndykov06@gmail.com>
2025-06-17 14:01:31 +0500

    Revert "INFR-1038: Keycloak 22 to 23"
    This reverts commit 148b7e09052bf005b1669f98926c3e96f672fea7.

--- apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
+++ apps/extensions/external-extensions/src/main/java/kz/kacd/sso/external/flow/login/ExtendedUsernamePasswordForm.java
@@ -54,9 +54,7 @@ public class ExtendedUsernamePasswordForm extends UsernamePasswordForm implement
             @Override
             public void action(AuthenticationFlowContext context) {
                 String username = extractUsername(context);
-                log.debug("Aza username is " + username);
                 boolean isIin = isIin(username);
-                log.debug("Aza isIin is " + isIin);
                 if (isIin) {
                     rewriteContextUsername(context, username);
                 }
@@ -84,7 +82,6 @@ public class ExtendedUsernamePasswordForm extends UsernamePasswordForm implement
     }
 
     private void rewriteContextUsername(AuthenticationFlowContext context, String iin) {
-        log.debug("Aza Rewrite context username: " + iin);
         String username = iin + "-" + ExternalRegistrationPage.CLIENT_PHYSICAL;
         setUsername(context, username);
     }
