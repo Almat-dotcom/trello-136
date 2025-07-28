4de6545c3fa6e8f71f7234ab950ea68a9bc2f061
Almat-dotcom <asagyndykov06@gmail.com>
2025-07-28 12:38:31 +0500

    INFR-1066: Improvements on the front in KeyCloak: validation of IIN and mail fields when entering

--- apps/themes/afr/src/lib/i18n/en.ts
+++ apps/themes/afr/src/lib/i18n/en.ts
@@ -59,7 +59,7 @@ export const en = {
     "missingFirstNameMessage": "Please specify first name.",
     "missingLastNameMessage": "Please specify last name.",
     "missingEmailMessage": "Please specify email.",
-    "missingUsernameMessage": "Please specify username.",
+    "missingUsernameMessage": "Please specify username. LLL",
     "missingPasswordMessage": "Please specify password.",
     "notMatchPasswordMessage": "Passwords don't match.",
     "error-empty": "Please specify value.",
