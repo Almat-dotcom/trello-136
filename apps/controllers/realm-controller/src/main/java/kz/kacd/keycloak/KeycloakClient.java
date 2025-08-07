b88d56fda56a3743eeedc34a4b7eaf4d63ba422c
Almat-dotcom <asagyndykov06@gmail.com>
2025-08-07 14:04:35 +0500

    INFR-1114: Bug: Клиенты не создаются после обновления Keycloak 23. KeycloakClient Consumes remove.

--- apps/controllers/realm-controller/src/main/java/kz/kacd/keycloak/KeycloakClient.java
+++ apps/controllers/realm-controller/src/main/java/kz/kacd/keycloak/KeycloakClient.java
@@ -5,23 +5,18 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/k8s-config")
+@Produces(MediaType.APPLICATION_JSON)            // ← сервер всё равно отдаёт JSON
 public interface KeycloakClient {
 
     @POST
     @Path("/realm/{name}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     Response updateRealm(@PathParam("name") String name);
 
     @POST
     @Path("/federation/{name}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     Response updateFederation(@PathParam("name") String name);
 
     @POST
     @Path("/client/{name}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     Response updateClient(@PathParam("name") String name);
 }
