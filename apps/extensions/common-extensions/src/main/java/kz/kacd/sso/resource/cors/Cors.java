7a36dd52732a5098d0b393eb3873d4532c7c0b92
Almat-dotcom <asagyndykov06@gmail.com>
2025-08-11 23:20:29 +0500

    INFR-1114:Cors add POST

--- apps/extensions/common-extensions/src/main/java/kz/kacd/sso/resource/cors/Cors.java
+++ apps/extensions/common-extensions/src/main/java/kz/kacd/sso/resource/cors/Cors.java
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 public class Cors {
 
     public static final long DEFAULT_MAX_AGE = TimeUnit.HOURS.toSeconds(1);
-    public static final String DEFAULT_ALLOW_METHODS = "GET, HEAD, OPTIONS";
+    public static final String DEFAULT_ALLOW_METHODS = "GET, HEAD, OPTIONS, POST";
     public static final String DEFAULT_ALLOW_HEADERS =
             "X-PINGOTHER, Origin, Accept, X-Requested-With, Content-Type, " +
                     "Access-Control-Request-Method, Access-Control-Request-Headers";
